6c66b0b4e4e1f94828ec2b9fef5ed98947f58e5b
aliasting <alias0305@gmail.com>
2023-10-03 13:55:31 +0800

    modify

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello, Flask!</h1><h2>Flask2級!</h2><p>這是段落1111</p>"
+    return render_template("index.html")
 
 @app.route("/hello_world")
 def hello_world():
